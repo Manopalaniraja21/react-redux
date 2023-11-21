552f52c7258381df459760a6c5502c6fdb2084b8
Manopalaniraja21 <127928332+Manopalaniraja21@users.noreply.github.com>
2023-11-21 11:47:30 +0530

    Add files via upload

---
+++ portal_project/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import InlineStyling from './day_2_ce/InlineStyling';
+
+function App() {
+  return (
+    <div className="App" data-testid="app">
+      <InlineStyling />
+    </div>
+  );
+}
+
+export default App;
